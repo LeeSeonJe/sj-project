ab5cbf8751adaf03fa57254714d136179943842b
LeeSeonJe <tjswp012@gmail.com>
2021-10-05 21:47:35 +0900

    first commit

---
+++ src/main/java/com/sj/sjproject/SjProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sj.sjproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sj/sjproject/SjProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sj.sjproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
